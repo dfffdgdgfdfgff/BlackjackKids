219523303b0852de630996a7b8a4a43c7f46e8a1
Oswin <ok.huizinga@student.alfa-college.nl>
2026-04-09 13:59:12 +0200

    Only shows the dealer's first card untill the player stands

--- game.js
+++ game.js
@@ -39,24 +39,28 @@ function displayDealer(instance) {
     switch(instance) {
         case "all":
             dealerHand.forEach(card => {
-            const img = document.createElement("img");
-            img.src = card.image;
-            img.style.width = "100px";
-            img.style.margin = "5px";
+                const img = document.createElement("img");
+                img.src = card.image;
+                img.style.width = "100px";
+                img.style.margin = "5px";
 
-            container.appendChild(img);
+                container.appendChild(img);
+            });
             return;
-        });
         case "one":
-            card = dealerHand[0]
-            const img = document.createElement("img");
-            img.src = card.image;
-            img.style.width = "100px";
-            img.style.margin = "5px";
-            container.appendChild(img);
+            const firstCard = dealerHand[0];
 
-            const backcard = document.createElement("img");
-            img.src = "cards/back.png"
+            const img1 = document.createElement("img");
+            img1.src = firstCard.image;
+
+            const img2 = document.createElement("img");
+            img2.src = "cards/back.png";
+
+            [img1, img2].forEach(img => {
+                img.style.width = "100px";
+                img.style.margin = "5px";
+                container.appendChild(img);
+            });
             return;
     }
 }
@@ -81,14 +85,28 @@ function stand() {
     document.getElementById("hitBtn").classList.add("hidden");
     document.getElementById("standBtn").classList.add("hidden");
 
-    dealerHit();
+    // Reveal dealer's cards
+    displayDealer("all");
+
+    switch (dealerWinCheck()) {
+        case "win":
+            playerWin();
+            return;
+        case "loss":
+            playerLoss();
+            return;
+        case "continue":
+            dealerHit();
+            return;
+    }
 }
 
-function dealerHit() {    
+function dealerHit() {
     setTimeout(function(){
+
         drawCard(1, dealerHand);
 
-        displayHand(dealerHand, "dealerHand");
+        displayDealer("all");
 
         switch (dealerWinCheck()) {
             case "win":
@@ -100,7 +118,6 @@ function dealerHit() {
             case "continue":
                 dealerHit();
                 return;
-        
         }
     }, 2000);
 }
@@ -152,20 +169,17 @@ function startGame() {
     drawCard(2, playerHand);
     drawCard(2, dealerHand);
 
-    document.getElementById("playerHandAnnounce").classList.remove("hidden");
-    document.getElementById("dealerHandAnnounce").classList.remove("hidden");
-
     displayHand(playerHand, "playerHand");
-    displayHand(dealerHand, "dealerHand");
+    displayDealer("one");
     showPlayBtns();
 
     if (playerWinCheck() && HandValue(dealerHand) == 21) {
-        console.log("Pushed!")
+        console.log("Pushed")
         playerWin();
         return;
     }
     if (playerWinCheck()) { // Checks if the player has blackjack
-        console.log("Blackjack!");
+        console.log("Blackjack");
         playerWin();
         return;
     }
@@ -174,6 +188,9 @@ function startGame() {
         playerLoss();
         return;
     }
+
+    document.getElementById("playerHandAnnounce").classList.remove("hidden");
+    document.getElementById("dealerHandAnnounce").classList.remove("hidden");
 }
 
 // Win & loss cases
@@ -196,6 +213,7 @@ function playerWin(){
     console.log("Win");
 }
 
+// Buttons & elements
 function returnToMenu(){
     window.location.reload()
 }
@@ -211,12 +229,12 @@ function showPlayBtns(){
     document.body.classList.add("game");
 }
 
-// event listeners
+// Event listeners
 document.getElementById("hitBtn").addEventListener("click", hit);
 document.getElementById("standBtn").addEventListener("click", stand);
 document.getElementById("playBtn").addEventListener("click", startGame);
 document.getElementById("lossReturnBtn").addEventListener("click", returnToMenu);
 document.getElementById("winReturnBtn").addEventListener("click", returnToMenu);
 
-
+// Logs
 console.log(playerHand);
